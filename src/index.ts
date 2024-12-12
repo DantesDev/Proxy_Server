37eff2eb9c1c54df81a4ba3709ae4972e4208809
DantesDev <lord.atos0411@gmail.com>
2024-12-12 09:58:50 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const PORT = 3300;
 
-const TARGET = 'https://cache-half-full-production.fly.dev';
+const TARGET = 'https://server.antfarm.finance';
 
 const app = express();
 app.use(
